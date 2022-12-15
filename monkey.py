361a6e774cebb6ebc522edc439d5504d6876f3d0
Carlo Terzaghi Tuck Schneider <69093510+carloterzaghi@users.noreply.github.com>
2022-12-14 22:45:04 -0300

    Delete monkey.py

--- monkey.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from discord import raw_models
-from discord.ext import commands
-import random
-
-def monkey(nick):
-    List = ["Por favor, " + nick + ", não seja monkey!", nick + ", você é um monkey", "Pare de ser monkey " + nick, "Meu deus " +nick+ ", você é monkey de mais"]
-    return random.choice(List)
