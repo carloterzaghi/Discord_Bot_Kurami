51518634c693a607d62cfece946164f14664b82c
Carlo Terzaghi Tuck Schneider <69093510+carloterzaghi@users.noreply.github.com>
2022-12-14 22:45:13 -0300

    Delete luiz.py

--- luiz.py
+++
@@ -1,6 +0,0 @@
-from discord.ext import commands
-import random
-
-def luiz(nick):
-    List = ["Cala boca " + nick, "Vai se fuder "+ nick, "Calaboca " + nick + " seu corno", "Da uma mamada " + nick, "Da uma sugada "+nick, "Primata do krl", "O de cima da o cu", "E falou o mama bola"]
-    return random.choice(List)
